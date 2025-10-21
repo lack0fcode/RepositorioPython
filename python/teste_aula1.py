a9e13e8fe726b3684e9dcf25a48c428d47f68b0c
Caue Felipe Trovatto Tragante <cauef.t.tragante@gmail.com>
2025-10-21 19:57:45 -0300

    aula2

---
+++ python/teste_aula1.py
@@ -0,0 +1,8 @@
+a = 2
+b = 12
+
+c = a + b
+print("soma = ", c)
+
+if(c >=6):
+    print("teste")
---
+++ python/teste_aula2.py
@@ -0,0 +1,8 @@
+nota1 = 6
+nota2 = 7
+nota3 = 10
+nota4 = 3
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print("Média = ", media)
+print("MeiaMedia =", media/2)
